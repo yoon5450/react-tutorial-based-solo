638545999e6142621fc6af951eb0510df3c3e4b3
yoon5450 <dbseodnd356@naver.com>
2025-07-02 20:14:19 +0900

    export component와 이벤트 부여

--- react-my/src/App.jsx
+++ react-my/src/App.jsx
@@ -2,25 +2,18 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import ShoppingList from "./ShoopingList";
 
 function MyButton() {
-  return <button>I'm a button</button>;
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  return <button onClick={handleClick}>Click me</button>;
 }
 
 function App() {
-  const products = [
-    // 여러 아이템 항목
-    { title: "Cabbage", id: 1 },
-    { title: "Garlic", id: 2 },
-    { title: "Apple", id: 3 },
-  ];
-
-  // 새로운 변수에 데이터를 삽입 ( 불변성 )
-  const listItems = products.map((product) => (
-    <li key={product.id}>{product.title}</li>
-  ));
-
-  return <ul>{listItems}</ul>;
+  return <MyButton />;
 }
 
 export default App;
