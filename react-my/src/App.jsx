4122ef6eb68bfd1f16cdb2059384515a54889aed
yoon5450 <dbseodnd356@naver.com>
2025-07-03 19:59:38 +0900

    usestate 주석과 사용해보기

--- react-my/src/App.jsx
+++ react-my/src/App.jsx
@@ -1,19 +1,31 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import ShoppingList from "./ShoopingList";
 
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton /> // 같은 컴포넌트 명으로 호출
+      <MyButton /> // 그러나 count 값은 각각 적용됨.
+    </div>
+  );
+}
+
 function MyButton() {
+  const [count, setCount] = useState(0); // 초기값을 0으로 설정
+
   function handleClick() {
-    alert("You clicked me!");
+    // 클릭할 때마다 count + 1
+    setCount(count + 1);
   }
 
-  return <button onClick={handleClick}>Click me</button>;
+  return (
+    <button onClick={handleClick}>
+      {" "}
+      // onclick 이벤트 부여 Clicked {count} times
+    </button>
+  );
 }
-
-function App() {
-  return <MyButton />;
-}
-
-export default App;
